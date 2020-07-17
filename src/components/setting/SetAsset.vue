d15c7ccddb2f216174db2e0480ed5eb60e001dbd
EunJaePark <design0728@naver.com>
2020-07-17 16:02:11 +0900

    update SettingPage 스타일, 코드(수정중)

--- src/components/setting/SetAsset.vue
+++ src/components/setting/SetAsset.vue
@@ -7,20 +7,28 @@
     <form action="">
       <div action="" class="total-goal-cont">
         <strong>총 목표 금액</strong>
-        <input type="text" placeholder="총 목표 금액을 입력해 주세요" />
+        <input
+          type="text"
+          placeholder="총 목표 금액을 입력해 주세요"
+          v-model="saveAsset.total"
+        />
       </div>
 
       <div action="" class="money-goal-cont">
         <strong>현금 목표 금액</strong>
-        <input type="text" placeholder="현금 목표 금액을 입력해 주세요" />
+        <input
+          type="text"
+          placeholder="현금 목표 금액을 입력해 주세요"
+          v-model="saveAsset.cash"
+        />
       </div>
 
       <div action="" class="credit-goal-cont">
         <strong>은행 별 자산 입력</strong>
-        <button>+</button>
+        <button @click.prevent="clickAddBank()">+</button>
         <ul>
-          <li>
-            <select name="bank">
+          <li v-for="(bankList, index) in banks" :key="index">
+            <select name="bank" v-model="saveAsset.bank">
               <option value="">은행선택</option>
               <option value="경남은행">경남은행</option>
               <option value="광주은행">광주은행</option>
@@ -48,18 +56,96 @@
             <input
               type="text"
               placeholder="해당 은행의 총 목표 금액을 입력해 주세요"
+              v-model="saveAsset.asset"
             />
+<<<<<<< Updated upstream
+=======
+            <!-- <button class="edit">수정</button> -->
+            <button class="remove" @click.prevent="clickRemoveBank(bankList)">
+              ✕
+            </button>
+>>>>>>> Stashed changes
           </li>
         </ul>
       </div>
-
-      <button>설정</button>
+      <!-- {{ banks }} -->
+      {{ saveAsset }}
+      <button
+        v-if="
+          this.$store.state.total === '' &&
+            this.$store.state.cash === '' &&
+            this.$store.state.bankAsset.bank === '' &&
+            this.$store.state.bankAsset.asset === 0
+        "
+        @click.prevent="clickSaveAsset()"
+      >
+        설정
+      </button>
+      <button
+        v-if="
+          this.$store.state.total !== '' ||
+            this.$store.state.cash !== '' ||
+            this.$store.state.bankAsset.bank !== '' ||
+            this.$store.state.bankAsset.asset !== 0
+        "
+        @click.prevent="clickSaveAsset()"
+      >
+        수정
+      </button>
     </form>
   </div>
 </template>
 
 <script>
+<<<<<<< Updated upstream
 export default {};
+=======
+import { saveTotal, saveCash, saveBankAsset } from '@/utils/cookies.js';
+
+export default {
+  data() {
+    return {
+      banks: [{ bank: '', asset: 0 }],
+      saveAsset: {
+        total: this.$store.state.total,
+        cash: this.$store.state.cash,
+        bank: '',
+        asset: 0,
+        // banks: [{ bank: '', asset: 0 }],
+      },
+    };
+  },
+  created() {
+    // --- cookie에 bank+asset를 합쳐 저장 해 놓은 것을 분리해서 data에 넣어줌. ---
+    // cookie에 저장된 bankAsset 불러와 변수 선언.
+    let getbankasset = this.$store.state.bankAsset.bank;
+    // bank(문자(한글+영어)만 추출)
+    this.saveAsset.bank = getbankasset.replace(/[0-9]/g, '');
+    // asset(숫자만 추출)
+    this.saveAsset.asset = getbankasset.replace(/[^0-9]/g, '');
+  },
+  methods: {
+    clickAddBank() {
+      // this.banks.push({ bank: '', asset: 0 });
+      // this.setAsset.push({ bank: '' });
+      // this.setAsset.push({ asset: 0 });
+      this.banks.push({ bank: '', asset: 0 });
+    },
+    clickRemoveBank(bankList) {
+      this.banks.$remove(bankList);
+    },
+    clickSaveAsset() {
+      // 총 목표 금액 저장
+      saveTotal(this.saveAsset.total);
+      // 현금 목표 금액 저장
+      saveCash(this.saveAsset.cash);
+      // 은행 별 자산 저장(은행명+자산금액 묶어서)
+      let bankAsset = `${this.saveAsset.bank}${this.saveAsset.asset}`;
+      saveBankAsset(bankAsset);
+    },
+  },
+};
+>>>>>>> Stashed changes
 </script>
 
 <style></style>
